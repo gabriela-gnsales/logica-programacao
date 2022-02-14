57ad63224eb9d96196c73adf4fb32a57c0136395
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-02-14 16:44:39 -0300

    Atualização 14/02/2022

--- aula-02/calculadora.js
+++ aula-02/calculadora.js
@@ -4,22 +4,21 @@ function calcular() {
     console.log("CLICK!!!")
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
+    // const num1 = document.querySelector("input#num1").value;
+    // const num2 = document.querySelector("input#num2").value;
     console.log(num1, num2);
     
-    const number1 = parseInt(num1);
-    const number2 = parseInt(num2);
-    console.log("number1:", typeof number1, "| number2:", typeof number2);
+    // const number1 = parseInt(num1);
+    // const number2 = parseInt(num2);
+    // console.log("number1:", typeof number1, "| number2:", typeof number2);
 
-    const soma = number1 + number2;
+    // const soma = number1 + number2;
+    const soma = parseInt(num1) + parseInt(num2);
     console.log("soma:", soma);
-    return soma;
+    document.querySelector(".resultado").innerHTML = soma;
+    // return soma;
 }
 
-dump(soma.innerHTML);
-
-// element.innerHTML = soma;
-
-// const resultado = document.write("soma");
 
 
 
